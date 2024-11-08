f5a2819250b95df91016cbab904f171d3b9c09d3
Omar Assidi <oia05@mail.aub.edu>
2024-11-09 00:01:56 +0300

    removed unused import

--- app/src/main/java/com/example/peoplelisting/ui/createpeople/view/CreatePersonScreen.kt
+++ app/src/main/java/com/example/peoplelisting/ui/createpeople/view/CreatePersonScreen.kt
@@ -8,11 +8,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
